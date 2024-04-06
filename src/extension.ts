1f304059d4e3f741657256c1466dab1fd117a12b
Gustavo Maia <vm.gustavo31@gmail.com>
2024-04-06 20:26:34 -0300

    create file with date name

--- src/extension.ts
+++ src/extension.ts
@@ -5,34 +5,57 @@ import * as vscode from 'vscode';
 
 export class ScratchpadsManager {
 
-	constructor() {}
-  
-	public async createScratchpad() {
-		// TODO: set a directory for the notes
-		let fullPath = path.join('/tmp/test-vsnote', 'noteUnique.md');
-  
-		// TODO: if file exists then open file
-		fs.writeFileSync(fullPath, '');
-  
-		const doc = await vscode.workspace.openTextDocument(fullPath);
-		vscode.window.showTextDocument(doc);
-  
-	  }
-	}
-  
+    constructor() { }
+
+    public async createScratchpad() {
+        // GET ROOT DIRECTORY FROM CONFIG
+        const config = vscode.workspace.getConfiguration('vsnote').get('createScratchPad');
+
+        // FORMAT DATE
+        let today = new Date();
+
+        let year = today.getFullYear().toString();
+        let month = String(today.getMonth() + 1).padStart(2, '0');
+        let day = String(today.getDay()).padStart(2, '0');
+        let hours = String(today.getHours()).padStart(2, '0');
+        let minutes = String(today.getMinutes()).padStart(2, '0');
+        let seconds = String(today.getSeconds()).padStart(2, '0');
+
+        let todayFmt = `${year}${month}${day}T${hours}${minutes}${seconds}`
+
+        // GET FILENAME FROM USER
+        const filenameFromUser = await vscode.window.showInputBox({
+            placeHolder: 'Enter a filename:',
+            value: `${todayFmt}-`
+        })
+
+        // CREATE AND OPEN FILE
+        let filename = `${filenameFromUser ?? ""}.md`
+
+        // TODO: set a directory for the notes
+        let fullPath = path.join('/tmp', filename);
+
+        // TODO: if file exists then open file
+        fs.writeFileSync(fullPath, '');
+
+        const doc = await vscode.workspace.openTextDocument(fullPath);
+        vscode.window.showTextDocument(doc);
+    }
+}
+
 
 export function activate(context: vscode.ExtensionContext) {
-	const scratchpadsManager = new ScratchpadsManager();
+    const scratchpadsManager = new ScratchpadsManager();
 
-	const commands: { [key: string]: (...args: any[]) => any } = {
-		'vsnote.createScratchPad': () => scratchpadsManager.createScratchpad(),
-	  };
-	
-	for (const command in commands) {
-		const cmd = vscode.commands.registerCommand(command, commands[command]);
-		context.subscriptions.push(cmd);
-	}
+    const commands: { [key: string]: (...args: any[]) => any } = {
+        'vsnote.createScratchPad': () => scratchpadsManager.createScratchpad(),
+    };
+
+    for (const command in commands) {
+        const cmd = vscode.commands.registerCommand(command, commands[command]);
+        context.subscriptions.push(cmd);
+    }
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
